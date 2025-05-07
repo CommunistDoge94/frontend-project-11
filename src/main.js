0dcd52bb50240f8a9b93a2f27c7696fb7e190497
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-07 17:46:26 +0300

    Попытка для деплоя

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.getElementById('rss-form');
+const feedsContainer = document.getElementById('feeds');
+const feeds = new Set();
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const url = document.getElementById('rss-url').value.trim();
+  
+  if (!url) return;
+  
+  if (feeds.has(url)) {
+    alert('Данная ссылка уже добавлена.');
+    return;
+  }
+
+  feeds.add(url);
+  renderFeeds();
+  form.reset();
+});
+
+function renderFeeds() {
+  feedsContainer.innerHTML = '';
+  feeds.forEach(url => {
+    const div = document.createElement('div');
+    div.textContent = url;
+    feedsContainer.appendChild(div);
+  });
+}
