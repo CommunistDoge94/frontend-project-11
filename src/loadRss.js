ccc12b6e8a8a16612bc905384bb4bf12e08f6260
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-07 19:42:10 +0300

    Загрузчик RSS

---
+++ src/loadRss.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import parseRss from './parseRss.js';
+
+const loadRss = (url) => {
+  const proxyUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
+
+  return axios.get(proxyUrl)
+    .then((response) => {
+      const rssContent = response.data.contents;
+      return parseRss(rssContent);
+    });
+};
+
+export default loadRss;
