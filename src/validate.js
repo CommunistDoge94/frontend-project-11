8827f83e4d951a3e6d3bfd9e835a8b0ccbf7663b
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-09 14:59:31 +0300

    Исправление отображения текста

--- src/validate.js
+++ src/validate.js
@@ -7,10 +7,11 @@ yup.setLocale({
   },
   mixed: {
     notOneOf: i18n.t('form.errors.duplicate'),
+    required: i18n.t('form.errors.required'),
   },
 });
 
 export default (url, existingFeeds) => {
-  const schema = yup.string().url().notOneOf([...existingFeeds]);
+  const schema = yup.string().required().url().notOneOf([...existingFeeds]);
   return schema.validate(url);
 };
